0e648e0016669f8f4ac1d64b02f849920f8ccef1
Kiran <84382139+Kiran-GC@users.noreply.github.com>
2026-04-29 14:42:57 +0000

    Update import statements in host.jsx to use include syntax

--- host/host.jsx
+++ host/host.jsx
@@ -1,7 +1,7 @@
-import "./core.jsxinc";
-import "./tools/layers.jsxinc";
-import "./tools/text.jsxinc";
-import "./tools/motion.jsxinc";
-import "./tools/media.jsxinc";
-import "./tools/templates.jsxinc";
-import "./entry.jsxinc";
+#include "core.jsxinc"
+#include "tools/layers.jsxinc"
+#include "tools/text.jsxinc"
+#include "tools/motion.jsxinc"
+#include "tools/media.jsxinc"
+#include "tools/templates.jsxinc"
+#include "entry.jsxinc"
